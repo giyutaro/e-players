3b63c14879354f0d0305987d7f964ca21504b2d3
carlos <cjr28rs@gmail.com>
2022-11-27 01:19:04 -0300

    e-players

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    let slideAtual = 1
+    let listaSlides = ["banner-lol", "banner-cp", "banner-ds"]
+
+    setInterval(mudarSlide, 2000);
+
+    function mudarSlide(){
+        //remover o slider anterior
+        if (slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual -1])
+        }
+        else{
+            $("#carrossel").removeClass(listaSlides[listaSlides.length -1])
+        }
+
+        //exibir o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual o slide atual
+        console.log("Slide atual é: ",slideAtual)
+        slideAtual ++
+
+        if (slideAtual > listaSlides.length -1){
+            slideAtual = 0
+        }
+    }
+
+
+    $("#barras").click(function(){
+        if ($("#menu").hasClass("menu-ativo")){
+            $("#menu").removeClass("menu-ativo")
+        }
+        else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
+
+function cadastrarNewsletter(){
+    let email = document.getElementById("campo-email").value
+    alert(email)
+}
+
+function mostrarmenu($event){
+    let menu = document.getElementById("menu")
+
+    if(menu.style.display == "none"){
+        menu.style.display = "flex"
+    }
+    else{
+        menu.style.display = "none"
+    }
+
+    event.preventDefault ();
+}
